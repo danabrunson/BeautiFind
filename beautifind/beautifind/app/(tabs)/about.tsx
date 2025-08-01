88f16041873765ba1c7c864a90f04a0ce28d4c7e
Dana Brunson <166191666+danabrunson@users.noreply.github.com>
2025-08-01 14:28:10 -0400

    Updates to favorites and about pages

--- beautifind/beautifind/app/(tabs)/about.tsx
+++ beautifind/beautifind/app/(tabs)/about.tsx
@@ -2,17 +2,33 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function About() {
   return (
-    <View style= {styles.viewStyle}>
-      <Text>All About BeautiFind!</Text>
+    <View style= {styles.container}>
+      <Text style={styles.title}>All About BeautiFind!</Text>
+      <Text style={styles.aboutText}>BeautiFind is an application designed to serve as a database for all your favorite beauty products-- whether that be for hair, skin, or makeup.
+        Search for your favorite products, add them to your favorites list, and find out where you can purchase them! 
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create ({
-  viewStyle : {
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+    alignItems: 'center',
+  },
+  
+  title: {
+    fontSize: 28,
+    fontWeight: 500,
+    color: 'black',
+    marginTop: 40,
+  },
+
+  aboutText: {
+    margin: 40,
+    padding: 40,
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+  },
 })
 
--- beautifind/beautifind/app/(tabs)/favorites.tsx
+++ beautifind/beautifind/app/(tabs)/favorites.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Match() {
   return (
     <View style={styles.container}>
-      <Text>Match products</Text>
+      <Text>Save products</Text>
     </View>
   );
 }
